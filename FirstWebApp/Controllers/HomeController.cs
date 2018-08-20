e38ad914f6d26dc1c41dd80b1c5774f4bec888ca
touir1 <touir.mat@gmail.com>
2018-08-20 18:41:08 +0100

    testing controller

---
+++ FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index()
+        {
+            return "hello world, this is the home view";
+        }
+    }
+}

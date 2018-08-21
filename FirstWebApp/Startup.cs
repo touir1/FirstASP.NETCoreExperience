9abbeff0efc70c94ea3c64b64b8a0184b03e1d01
touir1 <touir.mat@gmail.com>
2018-08-21 16:04:59 +0100

    testing appsettings

--- FirstWebApp/Startup.cs
+++ FirstWebApp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FirstWebApp
@@ -18,7 +19,7 @@ namespace FirstWebApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +28,8 @@ namespace FirstWebApp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = configuration["Greeting"];
+                await context.Response.WriteAsync(greeting);
             });
         }
     }

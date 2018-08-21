f90d5cdc1e800aee0f07c48fc1081f71a32aac2a
touir1 <touir.mat@gmail.com>
2018-08-21 16:34:10 +0100

    testing dependency injection

---
+++ FirstWebApp/IGreeter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace FirstWebApp
+{
+    public interface IGreeter
+    {
+        string getGreetingOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string getGreetingOfTheDay()
+        {
+            return "Greeting from Greeter : IGreeter -> getGreetingOfTheDay()\n" +
+                "using appsettings Greeting value: "+_configuration["Greeting"];
+        }
+    }
+}
--- FirstWebApp/Startup.cs
+++ FirstWebApp/Startup.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FirstWebApp
@@ -16,10 +11,11 @@ namespace FirstWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
@@ -28,7 +24,7 @@ namespace FirstWebApp
 
             app.Run(async (context) =>
             {
-                var greeting = configuration["Greeting"];
+                var greeting = greeter.getGreetingOfTheDay();
                 await context.Response.WriteAsync(greeting);
             });
         }

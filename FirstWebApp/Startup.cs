353a80636db352437b779ef6fb7beb2d1c5022fb
touir1 <touir.mat@gmail.com>
2018-08-21 17:13:35 +0100

    testing middlewares

--- FirstWebApp/Startup.cs
+++ FirstWebApp/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace FirstWebApp
 {
@@ -15,17 +16,41 @@ namespace FirstWebApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(next =>
+            {
+                return async context =>
+                {
+                    logger.LogInformation("Request incoming");
+                    if (context.Request.Path.StartsWithSegments("/mymiddleware"))
+                    {
+                        await context.Response.WriteAsync("from my middleware");
+                        logger.LogInformation("Request Handled");
+                    }
+                    else
+                    {
+                        await next(context);
+                        logger.LogInformation("Response outgoing");
+                    }
+                };
+            });
+
+            app.UseWelcomePage(new WelcomePageOptions
+            {
+                Path = "/welcome",
+            });
+
             app.Run(async (context) =>
             {
                 var greeting = greeter.getGreetingOfTheDay();
                 await context.Response.WriteAsync(greeting);
+                logger.LogInformation("from app.Run -> greeting");
             });
         }
     }

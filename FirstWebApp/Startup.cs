816890b7b9aa31991ecdcd24c46f8610d570ed36
touir1 <touir.mat@gmail.com>
2018-08-22 23:41:50 +0100

    Configuring the Entity Framework Services

--- FirstWebApp/Startup.cs
+++ FirstWebApp/Startup.cs
@@ -1,8 +1,11 @@
-﻿using FirstWebApp.Services;
+﻿using FirstWebApp.Data;
+using FirstWebApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -10,12 +13,22 @@ namespace FirstWebApp
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            services.AddDbContext<FirstWebAppDbContext>(
+                options => options.UseSqlServer(
+                    _configuration.GetConnectionString("FirstWebApp")));
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddMvc();
         }
 
